05c25f0270950e06f88f36e0c4b456fd64ca946d
Grady Morgan <gradymorgan@gmail.com>
2026-03-01 21:06:35 -0800

    Initial Commit

---
+++ NuimoController/NuimoControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuimoControllerApp.swift
+//  NuimoController
+//
+//  Created by Grady Morgan on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct NuimoControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NuimoControllerTests/NuimoControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NuimoControllerTests.swift
+//  NuimoControllerTests
+//
+//  Created by Grady Morgan on 3/1/26.
+//
+
+import Testing
+@testable import NuimoController
+
+struct NuimoControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
